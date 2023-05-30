d800ca5b4f29eb5e84bd11d95cdfb0ef3487693c
Zethi <diego1900196@gmail.com>
2023-05-30 09:02:56 -0400

    NavBar migrated to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { NavBar } from "@/components/ui/NavBar/NavBar";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { NavBar } from "@/components/ui/NavBar/NavBar";
-
 export default function Home() {
-  return (
-    <main>
-      <NavBar />
-    </main>
-  );
+  return <main></main>;
 }
