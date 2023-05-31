daa84ef7294ad0653279b630af2dc3c6564c4651
Zethi <diego1900196@gmail.com>
2023-05-31 16:18:21 -0400

    NavBar link refs added

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 import { NavLink } from "./NavLink";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
     <nav className="flex h-24 w-full items-center justify-between bg-white px-6 shadow-lg 2xl:px-80">
-      <div>
+      <Link href={"/"}>
         <Image
           src={"https://kayteam.org/wp-content/themes/KayTeam_Template/images/Recurso31.png"}
           alt="KayTeam"
           width={180}
           height={55}
         />
-      </div>
+      </Link>
       <div className="hidden lg:block">
-        <NavLink href={"/"} className={"mx-6 text-lg  xl:text-xl"}>
+        <NavLink href={"/about"} className={"mx-6 text-lg  xl:text-xl"}>
           ABOUT US
         </NavLink>
-        <NavLink href={"/"} className={"mx-6 text-lg  xl:text-xl"}>
+        <NavLink href={"/resources"} className={"mx-6 text-lg  xl:text-xl"}>
           OUR PRODUCTS
         </NavLink>
-        <NavLink href={"/"} className={"mx-6 text-lg  xl:text-xl"}>
+        <NavLink href={"https://github.com/KayTeam-Studio"} className={"mx-6 text-lg  xl:text-xl"}>
           PORTFOLIO
         </NavLink>
-        <NavLink href={"/"} className={"mx-6 text-lg  xl:text-xl"}>
+        <NavLink href={"/knowledge"} className={"mx-6 text-lg  xl:text-xl"}>
           KNOWLEDGE BASE
         </NavLink>
       </div>
