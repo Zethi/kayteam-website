4070bad5b29fe1b2394b62c7aa0ccd7e6bc7db47
Zethi <diego1900196@gmail.com>
2023-05-30 23:12:13 -0400

    Characteristics type card added

---
+++ src/components/ui/Card/CharacteristicsCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { FC } from "react";
+
+interface Props {
+  title: string;
+  description: string;
+  image: string;
+}
+export const CharacteristicsCard: FC<Props> = ({ image, title, description }) => {
+  return (
+    <div className="flex h-80 w-72 flex-col items-center justify-center rounded shadow-card">
+      <div className="mb-6 flex h-20 w-20 items-center justify-center rounded-lg bg-kt-orange">
+        <Image src={image} height={64} width={64} alt={"card-image"} />
+      </div>
+      <h3 className="mb-2 font-oswald text-3xl font-normal">{title}</h3>
+      <p className="mx-6 text-center font-lato text-base font-light">{description}</p>
+    </div>
+  );
+};
