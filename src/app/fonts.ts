b5d987813bd3f95f9b1da16a915f23ffc74c6a20
Zethi <diego1900196@gmail.com>
2023-06-14 19:42:32 -0400

    Next fonts added

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Oswald, Lato } from "next/font/google";
+
+export const oswald = Oswald({ weight: ["200", "300", "400", "500", "600", "700"], subsets: ["latin"] });
+export const lato = Lato({ weight: ["100", "300", "400", "700", "900"], subsets: ["latin"] });
