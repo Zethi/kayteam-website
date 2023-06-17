be6eb83586af2527b91e783217b62c298f47d4f9
Zethi <diego1900196@gmail.com>
2023-06-17 16:44:38 -0400

    incrementing max age header of images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   images: {
     domains: ["kayteam.org"]
+  },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate"
+          }
+        ]
+      }
+    ];
   }
 };
 
